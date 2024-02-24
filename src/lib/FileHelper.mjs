2a58dbc7fbfa78a732ff76e1892615890aee23c1
Jack Holloway <jack@mrprimate.co.uk>
2024-02-24 16:40:04 +0000

    Check upload not just browse

--- src/lib/FileHelper.mjs
+++ src/lib/FileHelper.mjs
@@ -210,7 +210,7 @@ const FileHelper = {
       const pathKey = `${targetDirectory}/${fileName}`;
       const filePath = `${parsedUploadPath.current}/${fileName}`;
 
-      if (!CONFIG.NOTELICKER.KNOWN.FILES.has(pathKey)) {
+      if (!CONFIG.NOTELICKER.KNOWN.FILES.has(pathKey) && game.user.can("FILES_UPLOAD")) {
         logger.debug(`Adding cached file ${fileName} to ${targetDirectory}`, {
           pathKey,
           filePath,
